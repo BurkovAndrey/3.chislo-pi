066d072e58d8309e0c989e9f0092235be69f2b18
Андрей Бурков <dryuxa_2000q@mail.ru>
2019-04-20 18:03:04 +0700

    Добавьте файлы проекта.

---
+++ chislo pi 1/chislo pi 1.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int signs; double pi; double tenth = 0.1;
+	printf("the precision with which to write pi\n");
+	scanf_s("%d", &signs);
+	double exactness = pow(tenth, signs);
+	double drob;
+	int i = 1;
+	pi = 0;
+	do
+	{
+		drob = (double)4 / (2 * i - 1);
+		pi -= pow(-1, i)*drob;
+		i++;
+	}
+	while (drob >= exactness);
+			printf("%.*lf",signs, pi);
+	return 0;
+}
